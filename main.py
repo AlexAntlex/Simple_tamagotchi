2cf10362551551fe31076d8951f1875da4db8bd3
AlexA <AlexAntlex@yandex.ru>
2022-07-31 14:06:26 +0300

    add new main

--- main.py
+++ main.py
@@ -1,100 +1,86 @@
-"""
+from datetime import datetime
 import sys
+
+import keyboard
 import pygame
 
+from config import screen, background_image, clock, SCR_SIZE, FOR_QUIT, FOR_PET, creatures, font
+from models.Buttons import simple_buttons
+from models.Dog import Dog
+
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
-clock = pygame.time.Clock()
-pygame.display.set_caption('game!')
-background_image = pygame.image.load('image.jpg')
+
+
+def pressed_key(event):
+    if event.name == '1' and event.event_type == 'down':
+        creature.set_hunger(True)
+    if event.name == '2' and event.event_type == 'down':
+        creature.set_fun(True)
+    if event.name == '3' and event.event_type == 'down':
+        creature.set_dirty(True)
+
+
+def draw_params(pet, param, text_xy):
+    text = font.render(f"{param}: ", True, (252, 252, 252))
+    text2 = font.render(str(pet), True, (252, 252, 252))
+    screen.blit(text, text_xy[0])
+    screen.blit(text2, text_xy[1])
+
+
+def pet():
+    global creature
+    creature = Dog(50, 20, 70)
+    creatures.add(creature)
+    time = datetime.today().strftime("%H.%M%S")
+    keyboard.hook(pressed_key)
+    while True:
+        new_time = datetime.today().strftime("%H.%M%S")
+        events = pygame.event.get()
+        screen.blit(background_image, (0, 0))
+        creatures.draw(screen)
+        if float(new_time) - float(time) > 0.0005:
+            time = datetime.today().strftime("%H.%M%S")
+            creature.set_fun()
+            creature.set_dirty()
+            creature.set_hunger()
+        if creature.ded:
+            screen.blit(font.render('Your pet died...', True, (252, 252, 252)), [350, 550])
+        draw_params(creature.hunger, "Hunger", ([20, 20], [190, 20]))
+        draw_params(creature.dirty, "Dirty", ([20, 80], [190, 80]))
+        draw_params(creature.fun, "Fun", ([20, 140], [190, 140]))
+
+        screen.blit(font.render("You can feed your pet by pressing '1'", True, (255, 255, 255)), [50, 650])
+        screen.blit(font.render("You can play with your pet by pressing '2'", True, (255, 255, 255)), [50, 750])
+        screen.blit(font.render("You can wash your pet by pressing '3'", True, (255, 255, 255)), [50, 850])
+        for i in events:
+            if i.type == pygame.QUIT:
+                sys.exit()
+        pygame.display.update()
+        clock.tick(60)
 
 
 def main():
     while True:
+        mouse = pygame.mouse.get_pos()
+        screen.blit(background_image, (0, 0))
         events = pygame.event.get()
         for i in events:
             if i.type == pygame.QUIT:
                 sys.exit()
-        screen.blit(background_image, (0, 0))
+
+            if i.type == pygame.MOUSEBUTTONDOWN:
+                if SCR_SIZE[0] / FOR_QUIT[0] <= mouse[0] <= SCR_SIZE[0] / FOR_QUIT[0] + 140 \
+                        and SCR_SIZE[1] / FOR_QUIT[1] <= mouse[1] <= SCR_SIZE[1] / FOR_QUIT[1] + 40:
+                    sys.exit()
+                if SCR_SIZE[0] / FOR_PET[0] <= mouse[0] <= SCR_SIZE[0] / FOR_PET[0] + 140 \
+                        and SCR_SIZE[1] / FOR_PET[1] <= mouse[1] <= SCR_SIZE[1] / FOR_PET[1] + 40:
+                    pet()
+
+        simple_buttons("Quit", FOR_QUIT[0], FOR_QUIT[1], mouse)
+        simple_buttons("Pet", FOR_PET[0], FOR_PET[1], mouse)
         pygame.display.update()
         clock.tick(60)
 
 
 if __name__ == "__main__":
     main()
-"""
-import sys
-from datetime import datetime
-from Creatures import Dog
-import keyboard
-
-
-def again_game():
-    print("Do you want to start the game again?\nPlease, answer 'Yes' or 'No'")
-    answer = input().lower()
-    if answer == "yes":
-        main()
-    else:
-        sys.exit()
-
-
-def pressed_key(event):
-    if event.name == '1' and event.event_type == 'down':
-        creature.set_hunger(True)
-        print(f"You have fed your pet! Hunger: {creature.hunger}")
-    if event.name == '2' and event.event_type == 'down':
-        creature.set_fun(True)
-        print(f"You played with a pet! Fun: {creature.fun}")
-    if event.name == '3' and event.event_type == 'down':
-        creature.set_dirty(True)
-        print(f"You have washed your pet! Dirty: {creature.dirty}")
-
-
-def game():
-    print("Your pet has appeared!")
-    print(f"Your pet's condition:\n Hunger: {creature.hunger}\n Fun: {creature.fun}\n Dirty: {creature.dirty}")
-    print('-'*10)
-    print("Pet parameters will change every 5 minutes of inactivity")
-    print("You can feed your pet by pressing '1'\nYou can play with your pet by pressing '2'\n"
-          "You can wash your pet by pressing '3'")
-
-    time = datetime.today().strftime("%H.%M%S")
-    keyboard.hook(pressed_key)
-
-    while creature.hunger != 0:
-        new_time = datetime.today().strftime("%H.%M%S")
-        if float(new_time) - float(time) > 0.0005:    # If 0.005 is here, then it means I'm still testing this code
-            time = datetime.today().strftime("%H.%M%S")
-            creature.set_fun()
-            creature.set_dirty()
-            creature.set_hunger()
-            if creature.hunger != 0:
-                if creature.hunger > 10:
-                    print(f"Your pet's condition::\n "
-                          f"Hunger: {creature.hunger}\n "
-                          f"Fun: {creature.fun}\n "
-                          f"Dirty: {creature.dirty}")
-                else:
-                    print(f"Your pet may starve to death!\n "
-                          f"Hunger: {creature.hunger}\n " 
-                          f"Fun: {creature.fun}\n "
-                          f"Dirty: {creature.dirty}")
-            else:
-                print("Game Over")
-                break
-
-
-def main():
-    global creature
-    creature = Dog(30, 60, 10)
-    game()
-
-
-if __name__ == "__main__":
-    print("Hi! Do you want to get yourself a pet?\nPlease, answer 'Yes' or 'No'")
-    answer = input().lower()
-    if answer == "yes":
-        main()
-    else:
-        sys.exit()
-
